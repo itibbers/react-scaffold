809ee079d7ffc918c53c3a786e864d53f9251f0b
iTibbers <jxn2014@gmail.com>
2020-05-11 22:57:47 +0800

    style: format

--- src/redux/reducers/counter.js
+++ src/redux/reducers/counter.js
@@ -6,19 +6,19 @@ const initState = {
 
 export default function reducer(state = initState, action) {
   switch (action.type) {
-  case INCREMENT:
-    return {
-      count: state.count + 1,
-    }
-  case DECREMENT:
-    return {
-      count: state.count - 1,
-    }
-  case RESET:
-    return {
-      count: 0,
-    }
-  default:
-    return state
+    case INCREMENT:
+      return {
+        count: state.count + 1,
+      }
+    case DECREMENT:
+      return {
+        count: state.count - 1,
+      }
+    case RESET:
+      return {
+        count: 0,
+      }
+    default:
+      return state
   }
 }
